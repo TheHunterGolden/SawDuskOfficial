d51a65d0422252a78d4c9224bc931e43c4aaee9e
TheHunterGolden <huntergolden0124@gmail.com>
2017-02-19 17:44:02 -0500

    fixed buutons

--- Assets/gameController.cs
+++ Assets/gameController.cs
@@ -6,7 +6,7 @@ using VRTK;
 public class gameController : MonoBehaviour {
 	public GameObject leftController;
 	public GameObject rightController;
-	public Transform dispenseLocation;
+	public Vector3 dispenseLocation;
 	public bool woodDispensed1;
 	public bool woodDispensed2;
 	public bool woodDispensed3;
@@ -26,7 +26,7 @@ public class gameController : MonoBehaviour {
 		woodDispensed2 = false;
 		woodDispensed3 = false;
 		woodDispensed4 = false;
-
+		dispenseLocation = new Vector3 (3, 10, -8);
 
 	}
 
@@ -39,35 +39,36 @@ public class gameController : MonoBehaviour {
 		Debug.Log ("touched");
 		if (leftTouched || rightTouched == buttonWood) {
 			if (woodDispensed1 == false) { 
-				Instantiate (wood, dispenseLocation);
+				Instantiate (wood, dispenseLocation, Quaternion.identity);
 				woodDispensed1 = true;
-			} else {
-				woodDispensed1 = false;
-			}
+			} 
+		} else {
+			woodDispensed1 = false;
 		}
+	
 		if (leftTouched || rightTouched == buttonSmallWood) {
 			if (woodDispensed2 == false) { 
-				Instantiate (wood, dispenseLocation);
+				Instantiate (wood, dispenseLocation, Quaternion.identity);
 				woodDispensed2 = true;
-			} else {
-				woodDispensed2 = false;
-			}
+			} 
+		}else {
+			woodDispensed2 = false;
 		}
 		if (leftTouched || rightTouched == buttonMediumWood) {
 			if (woodDispensed3 == false) { 
-				Instantiate (wood, dispenseLocation);
+				Instantiate (wood, dispenseLocation, Quaternion.identity);
 				woodDispensed3 = true;
-			} else {
-				woodDispensed3 = false;
-			}
+			} 
+		}else {
+			woodDispensed3 = false;
 		}
 		if (leftTouched || rightTouched == buttonLargeWood) {
 			if (woodDispensed4 == false) { 
-				Instantiate (wood, dispenseLocation);
+				Instantiate (wood, dispenseLocation, Quaternion.identity);
 				woodDispensed4 = true;
-			} else {
-				woodDispensed4 = false;
-			}
+			} 
+		}else {
+			woodDispensed4 = false;
 		}
 				
 	}
