49374e43db14896e49ae33c2e2d1c3041bdb304f
Michael Forrest <mikemforrest@gmail.com>
2017-02-26 14:26:41 -0500

    Glue FIx

--- Assets/Wood.cs
+++ Assets/Wood.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
+using VRTK.GrabAttachMechanics;
+using VRTK.SecondaryControllerGrabActions;
 
 public class Wood : MonoBehaviour {
 
 	// Use this for initialization
 	public GameObject empty;
+	public GameObject emptyInScene;
 	public bool isSticky;
 	void Start () {
 		isSticky = false;
@@ -27,32 +31,77 @@ public class Wood : MonoBehaviour {
 		if (target.gameObject.tag.Equals ("wood") == true) { 
 			//if this wood is sticky or the other wood object is sticky, connect
 			if (target.gameObject.GetComponent<Wood>().isSticky || this.isSticky){
-				Debug.Log("stuck");
-				target.gameObject.transform.SetParent(empty.transform); //sticks both wood objects together
-				//gameObject.transform.SetParent(empty.transform);
-				gameObject.transform.SetParent(empty.transform);
+				if ((gameObject.transform.parent == null) && (target.gameObject.transform.parent == null)){
+					if (((Mathf.Abs (GameObject.FindWithTag ("leftCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ().x)) < .5f) &&
+					    ((Mathf.Abs (GameObject.FindWithTag ("leftCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ().y)) < .5f) &&
+					    ((Mathf.Abs (GameObject.FindWithTag ("leftCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ().z)) < .5f) &&
+					    ((Mathf.Abs (GameObject.FindWithTag ("rightCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ().x)) < .5f) &&
+					    ((Mathf.Abs (GameObject.FindWithTag ("rightCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ().y)) < .5f) &&
+					    ((Mathf.Abs (GameObject.FindWithTag ("leftCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ().z)) < .5f)) {
+						Debug.Log (GameObject.FindWithTag ("leftCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ());
+						Debug.Log (GameObject.FindWithTag ("rightCont").GetComponent<VRTK_ControllerEvents> ().GetVelocity ());
+						emptyInScene = Instantiate (empty);
+						gameObject.GetComponent<VRTK_InteractableObject> ().ForceStopInteracting ();
+						target.gameObject.GetComponent<VRTK_InteractableObject> ().ForceStopInteracting ();
+						GameObject.FindWithTag ("leftCont").GetComponent<VRTK_InteractGrab> ().ForceRelease ();
+						GameObject.FindWithTag ("rightCont").GetComponent<VRTK_InteractGrab> ().ForceRelease ();
+						Destroy (gameObject.GetComponent<FixedJoint> ());
+						Destroy (target.gameObject.GetComponent<FixedJoint> ());
+						Destroy (gameObject.GetComponent<VRTK_FixedJointGrabAttach> ());
+						Destroy (target.gameObject.GetComponent<VRTK_FixedJointGrabAttach> ());
+						Destroy (gameObject.GetComponent<VRTK_InteractControllerAppearance> ());
+						Destroy (target.gameObject.GetComponent<VRTK_InteractControllerAppearance> ());
+						gameObject.GetComponent<VRTK_InteractableObject> ().enabled = false;
+						target.gameObject.GetComponent<VRTK_InteractableObject> ().enabled = false;
+						Destroy (gameObject.GetComponent<VRTK_InteractableObject> ());
+						Destroy (target.gameObject.GetComponent<VRTK_InteractableObject> ());
+						Destroy (target.gameObject.GetComponent<Rigidbody> ());
+						Destroy (gameObject.GetComponent<Rigidbody> ());
+						gameObject.transform.SetParent (emptyInScene.transform);
+						target.gameObject.transform.SetParent (emptyInScene.transform);
+						//emptyInScene.transform.SetParent(gameObject.transform);
+						//target.gameObject.transform.SetParent(emptyInScene.transform);
+						//Destroy(target.gameObject.GetComponent<Rigidbody>());
+						//Destroy(gameObject.GetComponent<Rigidbody>());
 
-				Destroy(target.gameObject.GetComponent<Rigidbody>());
-				Destroy(gameObject.GetComponent<Rigidbody>());
-				/*ConfigurableJoint joint = gameObject.AddComponent<ConfigurableJoint>();
+
+						Debug.Log ("stuck");
+						//target.gameObject.transform.SetParent(empty.transform); //sticks both wood objects together
+						//gameObject.transform.SetParent(empty.transform);
+						//gameObject.transform.SetParent(empty.transform);
+
+						//Destroy(target.gameObject.GetComponent<Rigidbody>());
+						//Destroy(gameObject.GetComponent<Rigidbody>());
+			
+						/*HingeJoint joint = gameObject.AddComponent<HingeJoint>();
+				joint.connectedAnchor.Set (0, 0, 0);
+				JointSpring hingeSpring = joint.spring;
+
+
+				hingeSpring.spring = 100000;
+				joint.spring = hingeSpring;
+				joint.useSpring = true;
+				joint.autoConfigureConnectedAnchor = false;
 				joint.angularXMotion = ConfigurableJointMotion.Locked;
 				joint.angularYMotion = ConfigurableJointMotion.Locked;
 				joint.angularZMotion = ConfigurableJointMotion.Locked;
 				joint.xMotion = ConfigurableJointMotion.Locked;
 				joint.yMotion = ConfigurableJointMotion.Locked;
 				joint.zMotion = ConfigurableJointMotion.Locked;
-				joint.projectionMode =JointProjectionMode.PositionAndRotation;
+				joint.projectionMode = JointProjectionMode.PositionAndRotation;
 
-				joint.connectedBody = target.gameObject.GetComponent<Rigidbody>();
-				*/
-				makeSticky();
-				//chainLink.GetComponentInChildren<Animation>().Play("Take 001"); //turns on the fan animation
+				joint.connectedBody = target.gameObject.GetComponent<Rigidbody>(); */
 
+						makeSticky ();
+					}
+				}
 			}
 
 		}
 
 	}
+
+		
 }
 
 
